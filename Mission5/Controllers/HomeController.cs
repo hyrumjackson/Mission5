2b875e585965813cf528a904cd6bb39584d73a0d
hyrumjackson <hyrumjjackson@gmail.com>
2024-02-08 14:15:11 -0700

    Add project files.

---
+++ Mission5/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission5.wwwroot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lessons()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission5/wwwroot/script.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#calculateButton').click(function ()
+    {
+        var hours = parseFloat($('#hoursInput').val());
+        var rate = parseFloat($('#rateDisplay').val());
+
+        if (hours > 0)
+        {
+            var total = hours * rate;
+            $('#totalCost').val(total.toFixed(2));
+        }
+        else
+        {
+            alert('Please enter a positive number of hours.');
+            $('#hoursInput').val(''); // Clear the input field
+            $('#totalCost').val(''); // Clear the total cost field
+        }
+    });
+});
